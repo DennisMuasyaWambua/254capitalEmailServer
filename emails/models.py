32a08054f1c4dc136f712803e531ab186ead78e8
Dennis wambua <muasyathegreat4@gmail.com>
2024-12-16 13:31:32 +0300

    attachment field added

--- emails/models.py
+++ emails/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.core.validators import FileExtensionValidator
 
 # Create your models here.
 class LoanApplicationModel(models.Model):
@@ -9,7 +10,14 @@ class LoanApplicationModel(models.Model):
   amount = models.DecimalField(max_digits=10, decimal_places=2)
   security_type = models.CharField(max_length=255)
   created_at = models.DateTimeField(auto_now_add=True)
-  attachment = models.FileField(required=False)
+  attachment = models.FileField( null=True,blank=True,validators=[
+            # Optional: Limit file extensions
+            FileExtensionValidator(
+                allowed_extensions=['pdf', 'doc', 'docx', 'jpg', 'jpeg', 'png']
+            )
+        ],
+        max_length=255  
+        )
 
   def __str__(self):
     return f"{self.name} - {self.loan_type}"
